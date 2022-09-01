eb10c0525bb8e98cc0cfab5d410c36e67151995a
Kwok yan Lau <dorislau@kwoks-mbp.lan>
2022-08-31 21:04:56 -0700

    second commit

---
+++ server.js
@@ -0,0 +1,37 @@
+// REQUIRE DEPENDENCIES
+const express = require('express');
+const budget = require("./models/budget");
+
+
+
+// INITIALIZE EXPRESS APP
+const app = express();
+const port = 3000;
+
+
+// DEFINE OUR ROUTES
+app.get("/budgets/", (req, res) => {
+    res.render("budget_index.ejs", {
+        allBudget: budget
+    })
+});
+
+
+app.get("/budgets/:index", (req, res) => {
+    res.render("budget_show.ejs", {
+        budgets: budget[req.params.index]
+    })
+});
+
+app.get("/budgets/new", (req, res)=>{
+    res.render("budget_new.ejs")
+});
+
+app.post("/budgets", (req, res) => {
+    console.log("Create route accessed!")
+    res.send("This route works")
+});
+    
+app.listen(port, () => {
+    console.log(`Working `, port)
+});
