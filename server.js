7f0088339a0ea6f0bd71302662dbd3417e0b9f40
Kwok yan Lau <dorislau@kwoks-mbp.lan>
2022-09-01 09:14:03 -0700

    Update all server.js

--- server.js
+++ server.js
@@ -1,38 +1,47 @@
-// REQUIRE DEPENDENCIES
+//////////////////////////////////// REQUIRE DEPENDENCIES ////////////////////////////////////
 const express = require('express');
 const budget = require("./models/budget");
 
-
-
-// INITIALIZE EXPRESS APP
+//////////////////////////////////// INITIALIZE EXPRESS APP ////////////////////////////////////
 const app = express();
 const port = 3000;
 
+//////////////////////////////////// DEFINE OUR ROUTES  ////////////////////////////////////
+// INDUCES
 
-// DEFINE OUR ROUTES
-app.get("/budgets/", (req, res) => {
+//////////////////////////////////// INDEX ////////////////////////////////////
+app.get("/budgets", (req, res) => {
     res.render("budget_index.ejs", {
         allBudget: budget
     })
 });
 
 
+//////////////////////////////////// NEW ////////////////////////////////////
+app.get("/budgets/new", (req, res) => {
+    res.render("budget_new.ejs")
+});
+
+//////////////////////////////////// DELETE ////////////////////////////////////
+
+//////////////////////////////////// UPDATE ////////////////////////////////////
+
+
+//////////////////////////////////// CREATE ////////////////////////////////////
+app.post("/budgets", (req, res) => {
+    console.log("Create route accessed!")
+    res.send("This route works")
+});
+
+////////////////////////////////////EDIT ////////////////////////////////////
+
+//////////////////////////////////// SHOW ////////////////////////////////////
 app.get("/budgets/:index", (req, res) => {
     res.render("budget_show.ejs", {
         budgets: budget[req.params.index]
     })
 });
 
-app.get("/budgets/new", (req, res)=>{
-    res.render("budget_new.ejs"
-    )
-});
-
-app.post("/budgets", (req, res) => {
-    console.log("Create route accessed!")
-    res.send("This route works")
-});
-    
 app.listen(port, () => {
     console.log(`Working `, port)
 });
